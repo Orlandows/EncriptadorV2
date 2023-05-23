d6afb2b1673e154211c6959dd7f91db3c4470447
Orlando <josuechali17@gmail.com>
2023-05-23 14:02:02 -0600

    Boton funcional de desencriptar

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ function encriptar(){
     }
 
 }
+
+function desencriptar (){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u")
+    
+    if(texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desncriptado con exito";
+        parrafo.textContent = "";
+        muneco.src = "./Imagenes/Saltando.jpg";
+    } else {
+        muneco.src = "./Imagenes/Muñeco.svg"
+        tituloMensaje.textContent = "Ningun mensaje encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("debes ingresaralgun texto");
+    }
+
+}
